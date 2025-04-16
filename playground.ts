c1a7f3d7d656c39fe57b192f913b208bb39b9619
CalvinWilkinson <kinsondigital@gmail.com>
2025-04-16 22:28:08 +0100

    chore: remove old project code

--- playground.ts
+++
@@ -1,8 +0,0 @@
-const _workingDir = Deno.env.get("WORKING_DIR") ?? "";
-const _token = Deno.env.get("GITHUB_TOKEN") ?? "";
-const _ownerName = Deno.env.get("OWNER_NAME") ?? "";
-const _repoName = Deno.env.get("REPO_NAME") ?? "";
-
-
-
-debugger;
