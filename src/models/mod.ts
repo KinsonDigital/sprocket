c4b0f2076dc63796f42ed184866eed32c773d5c4
CalvinWilkinson <kinsondigital@gmail.com>
2025-10-17 13:24:25 +0100

    config: add export for github models to the public API

---
+++ src/models/mod.ts
@@ -0,0 +1,26 @@
+/**
+ * Exports of GitHub related models.
+ * @module
+ */
+
+import type {
+	State,
+	LabelModel,
+	MilestoneModel,
+	PullRequestHeadOrBaseModel,
+	UserModel,
+	PullRequestInfoModel,
+	PullRequestModel,
+	RepoModel,
+} from "./github-models.ts";
+
+export type {
+	State,
+	LabelModel,
+	MilestoneModel,
+	PullRequestHeadOrBaseModel,
+	UserModel,
+	PullRequestInfoModel,
+	PullRequestModel,
+	RepoModel,
+};
